fdc87d9d63a22152a9f6ae7842ae6518e03cdbf0
rihabbel1 <rihab.belhibaa@gmail.com>
2021-12-07 08:55:52 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+declare let $: any;
+import { CalendarOptions } from '@fullcalendar/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calApp';
+  addEventForm: FormGroup;
+  submitted = false;
+  //Add user form actions
+  get f() { return this.addEventForm.controls; }
+  
+ onSubmit() {
+  
+  this.submitted = true;
+  // stop here if form is invalid and reset the validations
+  // this.addEventForm.get('title');
+  // this.addEventForm.get('title');
+  if (this.addEventForm.invalid) {
+      return;
+  }
+}
+constructor(private formBuilder: FormBuilder){}
+  title = 'angularadmintemplates';
+  calendarOptions: CalendarOptions;
+  ngOnInit() {
+    this.calendarOptions = {
+      initialView: 'dayGridMonth',
+      dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'event 1', date: '2020-11-05' },
+      { title: 'event 2', date: '2020-06-30' }
+    ]
+  };
+  this.addEventForm = this.formBuilder.group({
+    title: ['', [Validators.required]]
+    });
+}
+handleDateClick(arg) {
+  $("#myModal").modal('show');
+  $(".modal-title").text("");
+  $(".modal-title").text("Add Event at : "+arg.dateStr);
+    
+}
+hideForm(){
+  $('#myModal').modal('hide');
+  this.addEventForm.patchValue({ title : ""});
+  this.addEventForm.get('title').clearValidators();
+  this.addEventForm.get('title').updateValueAndValidity();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FullCalendarModule } from '@fullcalendar/angular'; 
+import dayGridPlugin from '@fullcalendar/daygrid'; 
 import { AppComponent } from './app.component';
+import interactionPlugin from '@fullcalendar/interaction'; 
+import { ReactiveFormsModule } from '@angular/forms';
+FullCalendarModule.registerPlugins([ 
+  dayGridPlugin,
+  interactionPlugin
+]);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullCalendarModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
